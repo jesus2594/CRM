253743f4ad594b975a402e3aede5d29853f4cb18
jesus2594 <jesusdavid890@hotmail.com>
2023-04-06 00:08:06 -0500

    Se agrega CRUD en JSON-SERVER

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
 import NuevoCliente, {action as nuevoClienteAction} from './pages/NuevoCliente'
 import Index, {loader as clientesLoader} from './pages/Index'
+import EditarCliente, {loader as editarClienteLoader, action as editarClienteAction } from './pages/EditarCliente'
+import ErrorPage from './components/ErrorPAge'
+import { action as eliminarClienteAction } from './components/Cliente'
+
 
 const router = createBrowserRouter([
   {
@@ -14,12 +18,25 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Index />,
-        loader: clientesLoader
+        loader: clientesLoader,
+        errorElement: <ErrorPage />
       },
       {
         path: '/clientes/nuevo',
         element: <NuevoCliente />,
-        action: nuevoClienteAction
+        action: nuevoClienteAction,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: '/clientes/:clienteId/editar',
+        element: <EditarCliente />,
+        loader: editarClienteLoader,
+        action: editarClienteAction,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: '/clientes/:clienteId/eliminar',
+        action: eliminarClienteAction
       }
     ]
 
--- src/pages/EditarCliente.jsx
+++ src/pages/EditarCliente.jsx
@@ -1,10 +1,21 @@
-import { useNavigate, Form, useActionData, redirect } from "react-router-dom"
-import Formulario from "../components/Formulario"
-import Error from "../components/Error"
-import { agregarCliente } from "../data/clientes"
+import { Form, useNavigate, useLoaderData, useActionData, redirect } from 'react-router-dom'
+import { obtenerCliente, actualizarCliente } from '../data/clientes'
+import Formulario from '../components/Formulario'
+import Error from '../components/Error'
 
-export async function action({request}){
-  const formData = await request.formData()
+export async function loader({params}) {
+    const cliente = await obtenerCliente(params.clienteId)
+    if(Object.values(cliente).length === 0){
+        throw new Response('', {
+            status: 404,
+            statusText: 'No hay resultados'
+        })
+    }
+    return cliente
+}
+
+export async function action({request, params}) {
+    const formData = await request.formData()
 
   const datos = Object.fromEntries(formData)
 
@@ -22,20 +33,22 @@ export async function action({request}){
   if(Object.keys(errores).length){
     return errores
   }
-
-   await agregarCliente(datos)
+  //actualizar
+   await actualizarCliente(params.clienteId, datos)
 
    return redirect('/')
 }
 
-function NuevoCliente() {
-  const errores = useActionData()
-  const navigate = useNavigate()
+function EditarCliente() {
+
+    const navigate = useNavigate()
+    const cliente = useLoaderData()
+    const errores = useActionData()
 
   return (
     <>
-      <h1 className='font-black text-4xl text-blue-900'>Nuevo Cliente</h1>
-      <p className='mt-3'>Llena todos los campos para registrar un nuevo cliente</p>
+      <h1 className='font-black text-4xl text-blue-900'>Editar Cliente</h1>
+      <p className='mt-3'>A continuacion podras modificar los datos de un cliente</p>
 
       <div className='flex justify-end'>
         <button
@@ -57,12 +70,14 @@ function NuevoCliente() {
         >
 
         
-        <Formulario />
+        <Formulario
+            cliente={cliente}
+        />
 
         <input 
           type="submit"
           className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
-          value="Registrar Cliente"  
+          value="Actualizar Cliente"  
         />
 
       </Form>
@@ -71,4 +86,4 @@ function NuevoCliente() {
   )
 }
 
-export default NuevoCliente
+export default EditarCliente
